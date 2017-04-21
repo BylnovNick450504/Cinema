e9d431649ded8fba70ade7c13fbe60fb28ada003
BylnovNick450504 <bylnov.nick@gmail.com>
2017-04-21 17:34:54 +0600

    add Person Role

---
+++ persistence/src/main/java/com/cinemaTicket/user/role/Role.java
@@ -0,0 +1,27 @@
+package com.cinemaTicket.user.role;
+
+import com.cinemaTicket.core.BaseEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "role")
+public class Role extends BaseEntity {
+    private String role;
+
+    public Role() {
+    }
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
